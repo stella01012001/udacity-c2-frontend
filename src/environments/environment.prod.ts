0787c546b8214cb516f25ee7af53486a61fd6986
Vo Ngoc Ty <vntya002@gmail.com>
2022-04-28 08:40:25 +0700

    update link backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://udacity-c2-resapi22222222.us-east-1.elasticbeantalk.com/api/v0'
 };
 
 /*
